d88b3565458ac5bcdfabd39937e62dd0737f7b83
GALI VENKATA GOPI <165005974+venkatagopigali@users.noreply.github.com>
2026-01-20 11:32:08 +0000

    commit -m "intial"

--- g.py
+++ g.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from google import genai
-client=genai.Client(api_key="AIzaSyDiI2q5R5fAfR9qocnSpFUYeGDZRJjLJzY")
+client=genai.Client(api_key="AIzaSyB3Kc6LJZj0sFYYOCFstJsTNYXPKkOIdvE")
 st.title("GENERATED_TEXT_PROJECT")
 text_box=st.text_input("ask any question")
 button=st.button("submit")
